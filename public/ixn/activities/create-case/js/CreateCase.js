1bce5aa1449d717c5345871adebc552f3f2918ee
Delpiero0424 <33252464+Delpiero0424@users.noreply.github.com>
2017-12-26 11:04:31 +0900

    Update CreateCase.js

--- public/ixn/activities/create-case/js/CreateCase.js
+++ public/ixn/activities/create-case/js/CreateCase.js
@@ -13,27 +13,7 @@ define( function( require ) {
 	var tokens;
 	var endpoints;
 	
-	//ヘッダーを定義
-	var headers = {
-		'Content-Type':'application/json',
-	}
-	var uid = {
-		'Ubbf28c454f516361872a7c7b58d365f2'
-	}
-	
-	//オプションを定義
-	var options = {
-	  url: 'https://master.laborot.com/api/push?uid=' + uid + '&cenarioid=22&test=1',
-	  method: 'POST',
-	  headers: headers,
-	  json: true,
-	  form: {"hoge":"fuga"}
-	}
-	
-	//リクエスト送信
-	request(options, function (error, response, body) {
-	  //コールバックで色々な処理
-	})
+
 	
     $(window).ready(onRender);
 
