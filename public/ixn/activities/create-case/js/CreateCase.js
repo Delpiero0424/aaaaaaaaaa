42c1e49bf18cfa746621c6b9d5771b5476868963
Delpiero0424 <33252464+Delpiero0424@users.noreply.github.com>
2017-12-26 13:49:09 +0900

    Update CreateCase.js

--- public/ixn/activities/create-case/js/CreateCase.js
+++ public/ixn/activities/create-case/js/CreateCase.js
@@ -87,7 +87,7 @@ define( function( require ) {
     };
     function fireRequest(){
 	//requestをrequire
-	var request = require('../../../../node_modules/request/request');
+	//var request = require('../../../../node_modules/request/request');
 		//ヘッダーを定義
 	var headers = {'Content-Type':'application/json'};
 	var uid = 'Ubbf28c454f516361872a7c7b58d365f2';
@@ -105,6 +105,9 @@ define( function( require ) {
 	request(options, function (error, response, body) {
 	  //コールバックで色々な処理
 	});
+    };
+    function getDataFromDE(id){
+	    
     };
 
     function gotoStep(step) {
